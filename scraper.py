8136b4c61e791b42f64a80cdef07367cf70ab9c0
Mateusz Roszkowski <mroszko2@gmail.com>
2018-03-06 00:15:33 +0100

    add individual page address search
    actual address of individual page is now being deduced from semester number (taken from calendar page).

--- scraper.py
+++ scraper.py
@@ -3,7 +3,14 @@ from lxml import html
 
 session = requests.Session()
 session.auth = ('user', 'password')
-url = 'http://studia.elka.pw.edu.pl/en/info'
-result = session.get(url)
-tree = html.fromstring(result.content)
-page = tree.xpath('//table//tr//text()') #find target element
+calendar_url = 'http://studia.elka.pw.edu.pl/cal/'
+
+result = session.get(calendar_url)
+calendar_tree = html.fromstring(result.content)
+semester = calendar_tree.xpath("//*[contains(text(),'Semestr')]/text()") #find target element
+temp = semester[0].split(' ')
+semester_number = temp[1]
+
+main_page_url = 'https://studia.elka.pw.edu.pl/en/' + semester_number + '/'
+result = session.get(main_page_url)
+main_page_tree = html.fromstring(result.content)
