d3676f0ebae8221c62ce8b497490320b77aca80e
Mateusz Roszkowski <mroszko2@gmail.com>
2018-03-06 23:57:31 +0100

    add subjects list
    scraping for list of subjects that student's attend

--- scraper.py
+++ scraper.py
@@ -7,10 +7,28 @@ calendar_url = 'http://studia.elka.pw.edu.pl/cal/'
 
 result = session.get(calendar_url)
 calendar_tree = html.fromstring(result.content)
-semester = calendar_tree.xpath("//*[contains(text(),'Semestr')]/text()") #find target element
+semester = calendar_tree.xpath("//*[contains(text(),'Semestr')]/text()")
 temp = semester[0].split(' ')
 semester_number = temp[1]
 
 main_page_url = 'https://studia.elka.pw.edu.pl/en/' + semester_number + '/'
 result = session.get(main_page_url)
 main_page_tree = html.fromstring(result.content)
+individual_page = main_page_tree.xpath("//tr//th[@class='nagl']/text()")
+
+length = len(individual_page) - 1
+
+my_subjects = []
+
+for x in range (35,length,2):
+    subject_code = individual_page[x]
+    subject_name = individual_page[x+1][:-1]
+    subject_data = (subject_code, subject_name)
+    my_subjects.append(subject_data)
+
+print("Courses you attend: ")
+
+for x in range(0, len(my_subjects)):
+    print(str(x+1) + '.', end = ' ')
+    print("[%s]" % (my_subjects[x][0]), end = ' ')
+    print(my_subjects[x][1])
